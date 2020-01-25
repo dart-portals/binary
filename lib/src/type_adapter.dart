1a98c63e7edd940b9495c73a4d981c7bf8e5a98b
Marcel Garus <marcel.garus@gmail.com>
2020-01-26 00:12:27 +0100

    make TypeAdapter override equals method

--- lib/src/type_adapter.dart
+++ lib/src/type_adapter.dart
@@ -22,4 +22,18 @@ abstract class TypeAdapter<T> {
   /// Registers this adapter for the given [typeId] on the given [registry].
   void _registerForId(int typeId, _TypeRegistryImpl registry) =>
       registry.registerAdapter<T>(typeId, this);
+
+  /// [TypeAdapter]s should have no internal state/fields whatsoever.
+  /// To clients, they should be merely a wrapper class around the [write] and
+  /// [read] method. That's why if you create two adapters of the same type,
+  /// they should be equal. We explicitly encode that here so that users don't
+  /// get weird errors if they try to register the "same" adapter twice:
+  ///
+  /// ```dart
+  /// AdapterForSomething().registerForId(0);
+  /// AdapterForSomething().registerForId(0); // Should not throw an error.
+  /// ```
+  bool operator ==(Object other) {
+    return runtimeType == other.runtimeType;
+  }
 }
